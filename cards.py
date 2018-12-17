95d61b36c8b7ede8d7d0af2d94028d4ee3e4a41e
Aaron <afink.2020@saintannsny.org>
2018-12-16 21:07:15 -0500

    Update cards.py

--- cards.py
+++ cards.py
@@ -13,4 +13,4 @@ name = input("What is your name?")
 # Additional challenges:
 # Tab in the phone number and position elements.
 # Try to split the motto into multiple lines if it's longer than the bars at the top and bottom of the card.
-# Try to make the name centered. There are various ways to do this, and none of them are easy - most involve using the len() method somewhere.
+# Try to make the name centered. There are various ways to do this, and none of them are easy - most involve using the len() function somewhere.
